bfc6bc945dcdc412e63be9d0590d49552ac35acb
raunak88 <raunak.kumar88@gmail.com>
2020-08-10 12:25:54 +0530

    Improved modal abstraction

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,28 +1,47 @@
-import React, { useEffect } from "react";
-import{createPortal} from 'react-dom'
-const Modal =  React.forwardRef((props, ref) => {
-  const modalRoot = document.getElementById("modal");
+import React, { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import "./Modal.scss";
+const Modal = (props) => {
+  const modalRoot = document.getElementById("root");
   const el = document.createElement("div");
 
   useEffect(() => {
     modalRoot.appendChild(el);
-    ref.current.focus()
   });
 
   useEffect(() => {
     return () => modalRoot.removeChild(el);
   });
 
+  const [open, setOpen] = useState(props.show);
+  const handleClose = () => {
+    setOpen(!open);
+  };
 
   const modalStructure = (
-    <div className="modal">
-      <div
-        style={{ height: "100px", width: "200px", background: "white" }}
-      >{props.children}</div>
-    </div>
-  );
-  return createPortal(modalStructure, el);
-})
+    <>
+      {open ? (
+        <div className="cover">
+          <div className="modal-area">
+            <div className="modal-content">
+              hi
+              <button className="primary" onClick={handleClose}>
+                Close Modal
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
 
+      <button className="primary" onClick={handleClose}>
+        Open Modal
+      </button>
+    </>
+  );
+
+  return createPortal(modalStructure, document.body);
+};
 
 export default Modal;

4eb2b688d54093149b04678410909375b0a0cca1
raunak88 <raunak.kumar88@gmail.com>
2020-08-13 13:31:02 +0530

    Completed split window for horizontal panes

--- src/SplitWindow/SplitWindow.js
+++ src/SplitWindow/SplitWindow.js
@@ -1,18 +1,21 @@
-import React,{useRef,useEffect,useState,createRef} from "react";
+import React, { useRef, useEffect, useState, createRef } from "react";
 import "./SplitWindow.scss";
 
 export default function SplitWindow(props) {
-  const topRef = createRef()
-  const mainWindow = createRef()
-
-  const [dragPosition,setDragPosition]=useState();
+  const topRef = createRef();
+  const mainWindow = createRef();
+  const separatorYPosition = React.useRef(null);
+  const [topHeight, setTopHeight] = useState(null);
 
   useEffect(() => {
-      if(topRef){
-          console.log(topRef.current.clientHeight)
-      }
-     
-  })
+    if (!topHeight) {
+      setTopHeight(topRef.current.clientHeight);
+      topRef.current.style.flex = "none";
+      return;
+    }
+
+    topRef.current.style.height = `${topHeight}px`;
+  }, [topHeight]);
 
   useEffect(() => {
     document.addEventListener("mousemove", onMouseMove);
@@ -24,22 +27,40 @@ export default function SplitWindow(props) {
     };
   });
 
-  const onMouseDown=()=>{
-      console.log("down")
-      
-  }
-  const onMouseUp=()=>{
-      console.log("up")
-  }
-  const onMouseMove=()=>{
-    console.log("move")
-}
+  const onMouseDown = (e) => {
+    console.log("down");
+    separatorYPosition.current = e.clientY;
+  };
+  const onMouseUp = (e) => {
+    separatorYPosition.current = null;
+    console.log("up");
+  };
+  const onMouseMove = (e) => {
+    if (!separatorYPosition.current) {
+      return;
+    }
+    const newTopHeight = topHeight + e.clientY - separatorYPosition.current;
+    separatorYPosition.current = e.clientY;
+    const splitPaneHeight = mainWindow.current
+      ? mainWindow.current.clientHeight
+      : "";
+    setTopHeight(newTopHeight);
+  };
 
   return (
-    <div class="split-window" ref={mainWindow}>
-      <div class="top-window" ref={topRef} >{props.topWindow}</div>
+    <div
+      className={
+        props.orientation === "vertical"
+          ? "split-window-vertical"
+          : "split-window-horizontal"
+      }
+      ref={mainWindow}
+    >
+      <div className="top-window" ref={topRef}>
+        {props.topWindow}
+      </div>
       <div className="separator" onMouseDown={onMouseDown}></div>
-      <div class="bottom-window">{props.bottomWindow}</div>
+      <div className="bottom-window">{props.bottomWindow}</div>
     </div>
   );
 }
